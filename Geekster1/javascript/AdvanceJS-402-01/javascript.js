64a6695b6d6bce586456df71ae331be6074a2b59
Garigasuraj <garigasuraj2127@gmail.com>
2023-01-05 01:08:56 +0530

    git change

--- Geekster1/javascript/AdvanceJS-402-01/javascript.js
+++ Geekster1/javascript/AdvanceJS-402-01/javascript.js
@@ -1,13 +1,14 @@
 const container_1 = document.getElementById("outer")
 const container_2 = document.getElementById("inner")
 
-init()
+
 function init(){
-    let html = ''
     window.addEventListener("keyup",(event)=>{
         document.getElementById("ll").style.display = "none"
         html = `  <div id="div_1"><h1>You pressed <span style="color: rgb(26, 185, 26);">${event.key}</span></h1></div>`
         container_1.innerHTML = html
         container_2.innerHTML = `<div id="div_2">${event.keyCode}</div>`
+        //console.log(aa)
     })
 }
+init()
